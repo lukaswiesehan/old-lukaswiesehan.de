b6febe1dc55ff1646bafea8298114a32a88453eb
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-03-10 10:55:48 +0100

    Fix card component

--- components/card.js
+++ components/card.js
@@ -1,25 +1,20 @@
-import {isMobile} from 'react-device-detect'
 import Tilt from 'react-parallax-tilt'
 
-export const Card = ({className, children}) => {
-  if(isMobile) {
-    return (
-      <div className={'lg:hidden rounded-xl overflow-hidden bg-warm-gray-300 dark:bg-blue-gray-700 ' + className}>
-        {children}
-      </div>
-    )
-  } else {
-    return (
-      <Tilt
-        className={'hidden lg:block relative group rounded-xl transition-shadow duration-300 hover:shadow-lg overflow-hidden bg-warm-gray-300 dark:bg-blue-gray-700 ' + className}
-        tiltMaxAngleX={4}
-        tiltMaxAngleY={2}
-        glareEnable={true}
-        glareMaxOpacity={0.3}
-      >
-        <div className="absolute w-full h-full bg-gradient-to-br transition-opacity duration-300 from-warm-gray-250 to-warm-gray-300 dark:from-blue-gray-600 dark:to-blue-gray-700 opacity-0 group-hover:opacity-100"/>
-        <div className="h-full">{children}</div>
-      </Tilt>
-    )
-  }
-}
+export const Card = ({className, children}) => (
+  <>
+    <div className={'lg:hidden rounded-xl overflow-hidden bg-warm-gray-300 dark:bg-blue-gray-700 ' + className}>
+      {children}
+    </div>
+    <Tilt
+      className={'hidden lg:block relative group rounded-xl transition-shadow duration-300 hover:shadow-lg overflow-hidden bg-warm-gray-300 dark:bg-blue-gray-700 ' + className}
+      tiltMaxAngleX={4}
+      tiltMaxAngleY={2}
+      glareEnable={true}
+      glareMaxOpacity={0.3}
+    >
+      <div className="absolute w-full h-full bg-gradient-to-br transition-opacity duration-300 from-warm-gray-250 to-warm-gray-300 dark:from-blue-gray-600 dark:to-blue-gray-700 opacity-0 group-hover:opacity-100"/>
+      <div className="h-full">{children}</div>
+    </Tilt>
+  </>
+)
+
